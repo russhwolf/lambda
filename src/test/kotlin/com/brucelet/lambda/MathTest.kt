21956db9ddc7cd0bf60cd8bf884263af07a0f216
Russell <russhwolf@gmail.com>
2017-06-11 23:13:49 -0400

    Convert Math tests to use parser

--- src/test/kotlin/com/brucelet/lambda/MathTest.kt
+++ src/test/kotlin/com/brucelet/lambda/MathTest.kt
@@ -2,37 +2,98 @@ package com.brucelet.lambda
 
 import org.junit.Test
 
-class MathTest {
+class MathTest : BaseParserTest() {
+    override fun Parser.initialize() {
+        parseLines("""
+        #def identity x = x
+        #def self_apply s = s s
+        #def apply func arg = func arg
+        #def select_first first second = first
+        #def select_second first second = second
+        #def make_pair e1 e2 c = c e1 e2
+        #def cond = make_pair
+        #def true = select_first
+        #def false = select_second
+        #def not x = x false true
+        #def and x y = x y false
+        #def or x y = x true y
+        #def zero = identity
+        #def succ = λn.λs.((s false) n)
+        #def iszero = λn.(n select_first)
+        #def pred = λn.(((iszero n) zero) (n select_second))
+        #def implies x y = x y true
+        #def equiv x y = x y (not y)
+        #def one = λs.((s false) zero)
+        #def two = λs.((s false) one)
+        #def three = λs.((s false) two)
+        #def four = λs.((s false) three)
+        #def five = λs.((s false) four)
+        #def six = λs.((s false) five)
+        #def seven = λs.((s false) six)
+        #def eight = λs.((s false) seven)
+        #def nine = λs.((s false) eight)
+        #def recursive f = λs.(f (s s)) λs.(f (s s))
+        #def add = recursive λf.λx.λy.(cond x (f (succ x) (pred y)) (iszero y))
+        #def mult = recursive λf.λx.λy.(cond zero (add x (f x (pred y))) (iszero y))
+        #def power = recursive λf.λx.λy.(cond one (mult x (f x (pred y))) (iszero y))
+        #def sub = recursive λf.λx.λy.(cond x (f (pred x) (pred y)) (iszero y))
+        #def abs_diff x y = add (sub x y) (sub y x)
+        #def equal x y = iszero (abs_diff x y)
+        #def greater x y = not (iszero (sub x y))
+        #def greater_or_equal x y = iszero (sub y x)
+        #def div = recursive λf.λx.λy.(cond zero (succ (f (sub x y) y)) (greater y x))
+        """)
+    }
+
     @Test
     fun addTest() {
-        "(($ADD $ONE) $TWO)".assertReducesTo("$THREE")
+        "add one two" assertResult "three"
     }
 
     @Test
     fun multTest() {
-        "(($MULT $TWO) $THREE)".assertReducesTo("$SIX")
+        "mult two three" assertResult "six"
     }
 
     @Test
     fun powerTest() {
-        "(($POWER $TWO) $ZERO)".assertReducesTo("$ONE")
-        "(($POWER $TWO) $ONE)".assertReducesTo("$TWO")
-        "(($POWER $TWO) $TWO)".assertReducesTo("$FOUR")
-        // TODO this failed to complete after 30 min
-//        "(($POWER $TWO) $THREE)".assertReducesTo("$EIGHT")
+        "power two zero" assertResult "one"
+        "power two one" assertResult "two"
+        "power two two" assertResult "four"
+    }
 
-        // TODO this fails for unknown reason
-//        assertParseAndReduceEquals("$EIGHT", "(($POWER $THREE) $TWO)")
+    @Test
+    fun twoCubedTest() {
+        // TODO this failed to complete after 30 min
+//        "power two three" assertResult "eight"
+    }
+
+    @Test
+    fun threeSquaredTest() {
+        // TODO this takes ~40s to complete
+//        "power three two" assertResult "nine"
     }
 
     @Test
     fun subTest() {
-        "(($SUB $FOUR) $TWO)".assertReducesTo("$TWO")
-        "(($SUB $ONE) $TWO)".assertReducesTo("$ZERO")
+        "sub four two" assertResult "two"
+        "sub one two" assertResult "zero"
     }
 
     @Test
     fun equalTest() {
-        "(($EQUAL $TWO) $THREE)".assertReducesTo("$FALSE")
+        "equal two two" assertResult "true"
+        "equal two three" assertResult "false"
+    }
+
+    @Test
+    fun greaterTest() {
+        "greater three two" assertResult "true"
+        "greater_or_equal two three" assertResult "false"
+    }
+
+    @Test
+    fun divTest() {
+        "div nine four" assertResult "two"
     }
 }
--- src/test/kotlin/com/brucelet/lambda/TestUtils.kt
+++ src/test/kotlin/com/brucelet/lambda/TestUtils.kt
@@ -5,5 +5,3 @@ import org.junit.Assert.assertNotEquals
 
 infix fun <T> T.assertEquals(value: T): Unit = assertEquals(value, this)
 infix fun <T> T.assertNotEquals(value: T): Unit = assertNotEquals(value, this)
-infix fun Expression.assertReducesTo(expression: Expression) = assertEquals(expression, reduce())
-infix fun String.assertReducesTo(expression: String) = parseExpression().assertReducesTo(expression.parseExpression())
