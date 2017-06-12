e6e56f9ea2e11a86d05d4c0449c5cd5bb804a953
Russell <russhwolf@gmail.com>
2017-06-11 21:19:21 -0400

    Convert Chapter 2 tests to use Parser

--- src/test/kotlin/com/brucelet/lambda/Chapter2Exercises.kt
+++ src/test/kotlin/com/brucelet/lambda/Chapter2Exercises.kt
@@ -2,31 +2,73 @@ package com.brucelet.lambda
 
 import org.junit.Test
 
-class Chapter2Exercises {
+class Chapter2Exercises : BaseParserTest() {
     // TODO 1?, 5, 6
 
+    override fun Parser.initialize() {
+        parseLines("""
+                #def identity = λx.x
+                #def self_apply = λs.(s s)
+                #def apply = λfunc.λarg.(func arg)
+                #def select_first = λfirst.λsecond.first
+                #def select_second = λfirst.λsecond.second
+                #def make_pair = λfirst.λsecond.λfunc.((func first) second)
+                """)
+    }
+
     @Test fun `2`() {
-        assertParseAndReduceEquals("λp.λq.p", "((λx.λy.(y x) λp.λq.p) λi.i)")
-        assertParseAndReduceEquals("λj.j", "(((λx.λy.λz.((x y) z) λf.λa.(f a)) λi.i) λj.j)")
-        assertParseAndReduceEquals("(λf.(f f) λf.(f f))", "(λh.((λa.λf.(f a) h) h) λf.(f f))")
-        assertParseAndReduceEquals("λb.λk.k", "((λp.λq.(p q) (λx.x λa.λb.a)) λk.k)")
-        assertParseAndReduceEquals("λb.b", "(((λf.λg.λx.(f (g x)) λs.(s s)) λa.λb.b) λx.λy.x)")
+        with(parser) {
+            parseLines("""
+                λp.λq.p
+                ((λx.λy.(y x) λp.λq.p) λi.i)
+                λj.j
+                (((λx.λy.λz.((x y) z) λf.λa.(f a)) λi.i) λj.j)
+                (λf.(f f) λf.(f f))
+                (λh.((λa.λf.(f a) h) h) λf.(f f))
+                λb.λk.k
+                ((λp.λq.(p q) (λx.x λa.λb.a)) λk.k)
+                λb.b
+                (((λf.λg.λx.(f (g x)) λs.(s s)) λa.λb.b) λx.λy.x)
+                """)
+        }
+        outputLines[0] assertEquals outputLines[1]
+        outputLines[2] assertEquals outputLines[3]
+        outputLines[4] assertEquals outputLines[5]
+        outputLines[6] assertEquals outputLines[7]
+        outputLines[8] assertEquals outputLines[9]
     }
 
     @Test fun `3`() {
-        assertParseAndReduceEquals("($IDENTITY a)", "(($APPLY ($APPLY $IDENTITY)) a)")
-        assertParseAndReduceEquals("($APPLY a)", "(λx.λy.((($MAKE_PAIR x) y) $IDENTITY) a)", from = "y", to = "arg")
-        assertParseAndReduceEquals("($IDENTITY a)", "(($SELF_APPLY ($SELF_APPLY $SELECT_SECOND)) a)")
+        with(parser) {
+            parseLines("""
+                (identity a)
+                ((apply (apply identity)) a)
+                (apply a)
+                (λx.λy.(((make_pair x) y) identity) a)
+                (identity a)
+                ((self_apply (self_apply select_second)) a)
+                """)
+        }
+        outputLines[0] assertEquals outputLines[1]
+        outputLines[2] assertActsAs outputLines[3]
+        outputLines[4] assertEquals outputLines[5]
     }
 
     @Test fun `4`() {
-        val makeTriplet = "λfirst.λsecond.λthird.λfunc.(((func first) second) third)".parseExpression()
-        val tripletFirst = "λfirst.λsecond.λthird.first".parseExpression()
-        val tripletSecond = "λfirst.λsecond.λthird.second".parseExpression()
-        val tripletThird = "λfirst.λsecond.λthird.third".parseExpression()
+        with(parser) {
+            parseLines("""
+                #def make_triplet = λfirst.λsecond.λthird.λfunc.(((func first) second) third)
+                #def triplet_first = λfirst.λsecond.λthird.first
+                #def triplet_second = λfirst.λsecond.λthird.second
+                #def triplet_third = λfirst.λsecond.λthird.third
+                ((((make_triplet a) b) c) triplet_first)
+                ((((make_triplet a) b) c) triplet_second)
+                ((((make_triplet a) b) c) triplet_third)
+                """)
+        }
 
-        "(((($makeTriplet a) b) c) $tripletFirst)".assertReducesTo("a")
-        "(((($makeTriplet a) b) c) $tripletSecond)".assertReducesTo("b")
-        "(((($makeTriplet a) b) c) $tripletThird)".assertReducesTo("c")
+        "((((make_triplet a) b) c) triplet_first)" assertResult "a"
+        "((((make_triplet a) b) c) triplet_second)" assertResult "b"
+        "((((make_triplet a) b) c) triplet_third)" assertResult "c"
     }
 }
--- src/test/kotlin/com/brucelet/lambda/TestUtils.kt
+++ src/test/kotlin/com/brucelet/lambda/TestUtils.kt
@@ -2,9 +2,11 @@ package com.brucelet.lambda
 
 import org.junit.Assert.assertEquals
 
-fun <T> T.assertEquals(value: T): Unit = assertEquals(value, this)
-fun Expression.assertReducesTo(expression: Expression) = assertEquals(expression, reduce())
-fun String.assertReducesTo(expression: String) = parseExpression().assertReducesTo(expression.parseExpression())
+infix fun <T> T.assertEquals(value: T): Unit = assertEquals(value, this)
+infix fun Expression.assertReducesTo(expression: Expression) = assertEquals(expression, reduce())
+infix fun String.assertReducesTo(expression: String) = parseExpression().assertReducesTo(expression.parseExpression())
+infix fun Expression.assertActsAs(that: Expression) = "($this a)".parseAndReduce() assertEquals "($that a)".parseAndReduce()
+infix fun String.assertActsAs(that: String) = this.parseExpression() assertActsAs that.parseExpression()
 
 fun assertParseAndReduceEquals(a: String, b: String) = assertEquals(a.parseAndReduce(), b.parseAndReduce())
 
